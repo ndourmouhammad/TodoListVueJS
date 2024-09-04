db2b177a2ebd5e7f8f29f86bb8da682eef592df5
Mouhammad ndour <ndourmouhammad15@gmail.com>
2024-09-04 18:44:38 +0000

    mise en place de todo list app

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,62 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="container">
+    <h1 class="text-center my-4">Gestion des tâches</h1>
+    <div class="mb-3">
+      <input v-model="newTaskTitle" class="form-control" placeholder="Titre de la tâche" />
+      <input v-model="newTaskDescription" class="form-control mt-2" placeholder="Description de la tâche" />
+      <button @click="addTask" class="btn btn-primary mt-3">Ajouter Tâche</button>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <ul class="list-group">
+      <li v-for="(task, index) in tasks" :key="index" class="list-group-item d-flex justify-content-between align-items-center">
+        <span :class="{ completed: task.completed }">{{ task.title }} - {{ task.description }}</span>
+        <i @click="toggleTask(index)" class="fas fa-check-circle " role="button"></i>
+      </li>
+    </ul>
+  </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      tasks: [
+        { title: "Tâche 1", description: "Description 1", completed: false },
+        { title: "Tâche 2", description: "Description 2", completed: false }
+      ],
+      newTaskTitle: "",
+      newTaskDescription: ""
+    };
+  },
+  methods: {
+    addTask() {
+      if (this.newTaskTitle && this.newTaskDescription) {
+        this.tasks.push({
+          title: this.newTaskTitle,
+          description: this.newTaskDescription,
+          completed: false
+        });
+        this.newTaskTitle = "";
+        this.newTaskDescription = "";
+      }
+    },
+    toggleTask(index) {
+      this.tasks[index].completed = !this.tasks[index].completed;
+    }
+  }
+};
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
+.completed {
+  text-decoration: line-through;
+  color: gray;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.btn-primary {
+  background-color: #017FC0;
+  border-color: #017FC0;
 }
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.btn-primary:hover {
+  background-color: #015f8d;
+  border-color: #015f8d;
 }
 </style>
